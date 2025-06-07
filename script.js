9fdedb862187ba0ec700b5dcbe7cb586e55ee934
yoss-beris <sanchezyoss50@gmail.com>
2025-06-07 17:43:15 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+  hamburger.classList.toggle('open');
+});
