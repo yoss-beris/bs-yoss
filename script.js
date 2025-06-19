d8ae71eab2bb81c2e441b67a78c7a13d4e82b539
yoss-beris <sanchezyoss50@gmail.com>
2025-06-19 14:48:38 -0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
-
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
-  hamburger.classList.toggle('open');
-});
